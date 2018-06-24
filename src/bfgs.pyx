3ad3c39f9aa9d3ead606fbf6505a6d1b7e8fae0d
KmolYuan <pyslvs@gmail.com>
2018-06-25 02:05:10 +0800

    Coding improvement.

--- src/bfgs.pyx
+++ src/bfgs.pyx
@@ -37,45 +37,39 @@ cdef extern from "solve.h":
     int solve(double **, int, Constraint *, int, int)
     void derivatives(double **, double *, int, Constraint *, int)
 
+from libc.stdlib cimport malloc, free
+
 
 cpdef test_kernel():
-    cdef double parameters[8]
-    parameters = [
+    cdef int i
+    cdef list params = [
         0, 0,
         5, 0,
         6, 5,
         6, 5,
     ]
-    cdef double *pparameters[8]
-    cdef int i
+    cdef double *parameters = <double *>malloc(len(params) * sizeof(double))
+    for i in range(len(params)):
+        parameters[i] = params[i]
+    cdef double **pparameters = <double **>malloc(len(params) * sizeof(double *))
     for i in range(8):
         pparameters[i] = &parameters[i]
     cdef double constants[3]
     constants = [30, 10, 24]
     
     cdef Point points[5]
-    points[0].x = pparameters[0]
-    points[0].y = pparameters[1]
-    
-    points[1].x = pparameters[2]
-    points[1].y = pparameters[3]
-    
-    points[2].x = pparameters[4]
-    points[2].y = pparameters[5]
-    
-    points[3].x = pparameters[6]
-    points[3].y = pparameters[7]
-    
-    points[4].x = &constants[0]
-    points[4].y = &constants[1]
-    
+    points = [
+        [pparameters[0], pparameters[1]],
+        [pparameters[2], pparameters[3]],
+        [pparameters[4], pparameters[5]],
+        [pparameters[6], pparameters[7]],
+        [&constants[0], &constants[1]],
+    ]
     cdef Line lines[2]
-    lines[0].p1 = &points[0]
-    lines[0].p2 = &points[1]
-    
-    lines[1].p1 = &points[1]
-    lines[1].p2 = &points[2]
-    
+    lines = [
+        [&points[0], &points[1]],
+        [&points[1], &points[2]],
+    ]
     cdef Constraint cons[4]
     cons = [
         HorizontalConstraint(&lines[0]),
@@ -87,7 +81,7 @@ cpdef test_kernel():
     for i in range(5):
         print("Point {}: ({}, {})".format(i, points[i].x[0], points[i].y[0]))
     
-    if solve(pparameters, 8, cons, 4, Rough) == Succsess:
+    if solve(pparameters, len(params), cons, len(cons), Rough) == Succsess:
         print("A good Solution was found.")
     else:
         print("No valid Solutions were found from this start point.")
@@ -95,8 +89,14 @@ cpdef test_kernel():
     for i in range(5):
         print("Point {}: ({}, {})".format(i, points[i].x[0], points[i].y[0]))
     
-    cdef double gradF[5]
-    gradF = [0] * 5
-    derivatives(pparameters, gradF, 5, cons, 4)
-    for i in range(5):
+    cdef list zeros = [0] * 5
+    cdef double *gradF = <double *>malloc(len(zeros) * sizeof(double))
+    for i in range(len(zeros)):
+        gradF[i] = zeros[i]
+    derivatives(pparameters, gradF, len(zeros), cons, len(cons))
+    for i in range(len(zeros)):
         print("GradF[{}]: {}".format(i, gradF[i]))
+    
+    free(parameters)
+    free(pparameters)
+    free(gradF)
