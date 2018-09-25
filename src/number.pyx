88925cf8096abfe0c2bb69f25cc5eb0f110471d6
KmolYuan <pyslvs@gmail.com>
2018-09-25 13:18:21 +0800

    Update number synthesis formula.

--- src/number.pyx
+++ src/number.pyx
@@ -8,45 +8,58 @@
 # __license__ = "AGPL"
 # __email__ = "pyslvs@gmail.com"
 
-from itertools import product
+
+cdef list product(object pool, int repeat=1):
+    """Product function as same as iteration tools."""
+    cdef list x, tmp_list
+    cdef int i, y
+    cdef list result = [[]]
+    for i in range(repeat):
+        tmp_list = []
+        for x in result:
+            for y in pool:
+                tmp_list.append(x + [y])
+        result = tmp_list
+    return result
 
 
-cdef inline int Max(int NL, int NJ):
+cdef inline int mmax(int nl, int nj):
     """Find max number of joint on each link.
     
-    + NL <= NJ and NJ <= (2 * NL - 3)
-    + (2 * NL - 3) <= NJ and NJ <= (NL * (NL - 1) / 2)
+    + nl <= nj and nj <= (2 * nl - 3)
+    + (2 * nl - 3) <= nj and nj <= (nl * (nl - 1) / 2)
     + other exceptions (return -1).
     """
-    if NL <= NJ and NJ <= (2 * NL - 3):
-        return NJ - NL + 2
-    if NL == NJ == 0:
+    if nl <= nj <= (2 * nl - 3):
+        return nj - nl + 2
+    if nl == nj == 0:
         return -1
-    if (2 * NL - 3) <= NJ and NJ <= (NL * (NL - 1) / 2):
-        return NL - 1
+    if (2 * nl - 3) <= nj <= (nl * (nl - 1) / 2):
+        return nl - 1
     return -1
 
 
-cdef inline int sum_factors(tuple factors):
+cdef inline int sum_factors(list factors):
     """F0*N2 + F1*N3 + F2*N4 + ... + Fn*N(n+2)"""
     cdef int factor = 0
-    for i, f in enumerate(factors):
-        factor += f * (i + 2)
+    cdef int i
+    for i in range(len(factors)):
+        factor += factors[i] * (i + 2)
     return factor
 
 
-cpdef tuple number_synthesis(int NL, int NJ):
+cpdef tuple number_synthesis(int nl, int nj):
     cdef list result = []
-    cdef int Mmax = Max(NL, NJ)
-    if Mmax == -1:
+    cdef int m_max = mmax(nl, nj)
+    if m_max == -1:
         raise Exception("incorrect mechanism.")
     cdef int i, p
-    cdef tuple symbols, answer
-    for symbols in product(range(NL + 1), repeat = (Mmax - 2)):
-        NLMmax = NL - sum(symbols)
-        if NLMmax < 0:
+    cdef list symbols
+    for symbols in product(range(nl + 1), m_max - 2):
+        nl_m_max = nl - sum(symbols)
+        if nl_m_max < 0:
             continue
-        answer = symbols + (NLMmax,)
-        if sum_factors(answer) == (2 * NJ):
-            result.append(answer)
+        symbols.append(nl_m_max)
+        if sum_factors(symbols) == (2 * nj):
+            result.append(symbols)
     return tuple(result)
