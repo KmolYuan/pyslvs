7e5459aff4eceb473794fecddaf7ac8ada132a15
KmolYuan <pyslvs@gmail.com>
2018-11-16 23:40:05 +0800

    Refactor unit test functions.

--- test_pyslvs.py
+++ test_pyslvs.py
@@ -9,11 +9,9 @@ __email__ = "pyslvs@gmail.com"
 
 import unittest
 from unittest import TestCase
-from typing import Tuple, List
 
 # For necessary testing modules.
 from math import sqrt, radians, isclose
-from pmks import VPoint
 from bfgs import vpoint_solving
 from tinycadlib import (
     Coordinate,
@@ -36,77 +34,64 @@ from _parser import parse_vpoints
 from examples import example_list
 
 
-def vpoints_object() -> Tuple[List[VPoint], Tuple[Tuple[int, int]]]:
-    """Example: Jansen's linkage (Single)."""
-    expr, inputs = example_list["Jansen's linkage (Single)"]
-    return parse_vpoints(expr), inputs
+planar_object = Planar({
+    'Driver': {'P0': (-70, -70, 50)},
+    'Follower': {'P1': (70, -70, 50)},
+    'Target': {'P4': [
+        (60.3, 118.12),
+        (31.02, 115.62),
+        (3.52, 110.62),
+        (-25.77, 104.91),
+        (-81.49, 69.19),
+        (-96.47, 54.906),
+        (-109.34, 35.98),
+        (-121.84, 13.83),
+        (-127.56, -20.09),
+        (-128.63, -49.74),
+        (-117.56, -65.45),
+    ]},
+    'Expression':
+        "PLAP[P0,L0,a0](P2);"
+        "PLLP[P2,L1,L2,P1](P3);"
+        "PLLP[P2,L3,L4,P3](P4)",
+    'constraints': [('P0', 'P1', 'P2', 'P3')],
+    'upper': [100., 100., 100., 100., 100., 360.],
+    'lower': [5., 5., 5., 5., 5., 0.],
+})
 
 
 class CoreTest(TestCase):
 
     """Testing Cython libs."""
 
-    def planar_object(self) -> Planar:
-        """Test-used mechanism for algorithm."""
-        return Planar({
-            'Driver': {'P0': (-70, -70, 50)},
-            'Follower': {'P1': (70, -70, 50)},
-            'Target': {'P4': [
-                (60.3, 118.12),
-                (31.02, 115.62),
-                (3.52, 110.62),
-                (-25.77, 104.91),
-                (-81.49, 69.19),
-                (-96.47, 54.906),
-                (-109.34, 35.98),
-                (-121.84, 13.83),
-                (-127.56, -20.09),
-                (-128.63, -49.74),
-                (-117.56, -65.45),
-            ]},
-            'Expression':
-                "PLAP[P0,L0,a0](P2);"
-                "PLLP[P2,L1,L2,P1](P3);"
-                "PLLP[P2,L3,L4,P3](P4)",
-            'constraints': [('P0', 'P1', 'P2', 'P3')],
-            'upper': [100., 100., 100., 100., 100., 360.],
-            'lower': [5., 5., 5., 5., 5., 0.],
-        })
-
     def test_plap(self):
         """Test for PLAP function."""
-        A = Coordinate(0, 0)
-        B = Coordinate(50, 0)
-        x, y = PLAP(A, 50 * sqrt(2), radians(45), B)
+        x, y = PLAP(Coordinate(0, 0), 50 * sqrt(2), radians(45), Coordinate(50, 0))
         self.assertTrue(isclose(x, 50))
         self.assertTrue(isclose(y, 50))
 
     def test_pllp(self):
         """Test for PLLP function."""
-        A = Coordinate(-30, 0)
-        B = Coordinate(30, 0)
-        x, y = PLLP(A, 50, 50, B)
+        c1 = Coordinate(-30, 0)
+        c2 = Coordinate(30, 0)
+        x, y = PLLP(c1, 50, 50, c2)
         self.assertTrue(isclose(x, 0))
         self.assertTrue(isclose(y, 40))
-        x, y = PLLP(A, 30, 30, B)
+        x, y = PLLP(c1, 30, 30, c2)
         self.assertTrue(isclose(x, 0))
         self.assertTrue(isclose(y, 0))
-        x, y = PLLP(A, 90, 30, B)
+        x, y = PLLP(c1, 90, 30, c2)
         self.assertTrue(isclose(x, 60))
         self.assertTrue(isclose(y, 0))
 
     def test_plpp(self):
         """Test for PLPP function."""
-        A = Coordinate(0, 0)
-        B = Coordinate(0, -3)
-        C = Coordinate(3/2, 0)
-        x, y = PLPP(A, sqrt(5), B, C)
+        x, y = PLPP(Coordinate(0, 0), sqrt(5), Coordinate(0, -3), Coordinate(3/2, 0))
         self.assertTrue(isclose(x, 2))
         self.assertTrue(isclose(y, 1))
 
     def test_pxy(self):
-        A = Coordinate(80, 90)
-        x, y = PXY(A, 40, -20)
+        x, y = PXY(Coordinate(80, 90), 40, -20)
         self.assertTrue(isclose(x, 120))
         self.assertTrue(isclose(y, 70))
 
@@ -116,11 +101,11 @@ class CoreTest(TestCase):
         + 'topo' function.
         + 'Graph' class.
         """
-        G = Graph([(0, 1), (0, 4), (1, 5), (2, 3), (2, 4), (3, 5), (4, 5)])
-        H = Graph([(0, 2), (0, 4), (1, 3), (1, 4), (2, 5), (3, 5), (4, 5)])
-        I = Graph([(0, 1), (0, 2), (1, 4), (2, 5), (3, 4), (3, 5), (4, 5)])
-        self.assertTrue(G.is_isomorphic(H))
-        self.assertFalse(G.is_isomorphic(I))
+        g1 = Graph([(0, 1), (0, 4), (1, 5), (2, 3), (2, 4), (3, 5), (4, 5)])
+        g2 = Graph([(0, 2), (0, 4), (1, 3), (1, 4), (2, 5), (3, 5), (4, 5)])
+        g3 = Graph([(0, 1), (0, 2), (1, 4), (2, 5), (3, 4), (3, 5), (4, 5)])
+        self.assertTrue(g1.is_isomorphic(g2))
+        self.assertFalse(g1.is_isomorphic(g3))
 
         answers = []
 
@@ -128,7 +113,7 @@ class CoreTest(TestCase):
         for c_j in contracted_link(type_1):
             answer, _ = topo(type_1, c_j)
             answers.extend(answer)
-        self.assertEqual(len(answers), 2)
+        self.assertEqual(2, len(answers))
         answers.clear()
 
         answers_degenerate = []
@@ -138,8 +123,8 @@ class CoreTest(TestCase):
                 answers.extend(answer)
                 answer, _ = topo(type_2, c_j, 2)
                 answers_degenerate.extend(answer)
-        self.assertEqual(len(answers), 16)
-        self.assertEqual(len(answers_degenerate), 40)
+        self.assertEqual(16, len(answers))
+        self.assertEqual(40, len(answers_degenerate))
 
     def test_solving(self):
         """Test triangular formula solving.
@@ -148,7 +133,8 @@ class CoreTest(TestCase):
         + Test data collecting function.
         + Test expression solving function.
         """
-        vpoints, inputs = vpoints_object()
+        expr, inputs = example_list["Jansen's linkage (Single)"]
+        vpoints = parse_vpoints(expr)
         self.assertTrue(len(vpoints) == 8)
         exprs = vpoints_configure(vpoints, inputs)
         mapping = {n: f'P{n}' for n in range(len(vpoints))}
@@ -167,7 +153,7 @@ class CoreTest(TestCase):
             ('L10', 65.69940106271898),
         ):
             self.assertTrue(isclose(data_dict[link], link_length))
-        self.assertEqual(dof, 1)
+        self.assertEqual(1, dof)
         result = expr_solving(exprs, mapping, vpoints, [0.])
         x, y = result[-1]
         self.assertTrue(isclose(x, -43.17005515543241))
@@ -175,7 +161,8 @@ class CoreTest(TestCase):
 
     def test_bfgs(self):
         """Test Sketch Solve kernel."""
-        vpoints, inputs = vpoints_object()
+        expr, _ = example_list["Jansen's linkage (Single)"]
+        vpoints = parse_vpoints(expr)
         result = vpoint_solving(vpoints, [(0, 1, 0.)])
         x, y = result[-1]
         self.assertTrue(isclose(round(x, 2), -43.17))
@@ -183,16 +170,16 @@ class CoreTest(TestCase):
 
     def test_number_synthesis(self):
         """Test Number Synthesis function."""
-        for NL, NJ in [(4, 4), (6, 7), (8, 9), (10, 12)]:
-            for factors in number_synthesis(NL, NJ):
+        for nl, nj in [(4, 4), (6, 7), (8, 9), (10, 12)]:
+            for factors in number_synthesis(nl, nj):
                 count = 0
                 for i, factor in enumerate(factors):
                     count += factor * (i + 2)
-                self.assertEqual(int(count / 2), NJ)
+                self.assertEqual(nj, int(count / 2))
 
     def test_algorithm_rga(self):
         """Real-coded genetic algorithm."""
-        fun1 = Genetic(self.planar_object(), {
+        _, t_f = Genetic(planar_object, {
             'maxTime': 1,
             'report': 10,
             # Genetic
@@ -201,12 +188,12 @@ class CoreTest(TestCase):
             'pMute': 0.05,
             'pWin': 0.95,
             'bDelta': 5.,
-        })
-        fun1.run()
+        }).run()
+        self.assertTrue(10 >= t_f[1][0] - t_f[0][0])
 
     def test_algorithm_firefly(self):
         """Firefly algorithm."""
-        fun2 = Firefly(self.planar_object(), {
+        _, t_f = Firefly(planar_object, {
             'maxTime': 1,
             'report': 10,
             # Firefly
@@ -215,12 +202,12 @@ class CoreTest(TestCase):
             'betaMin': 0.2,
             'gamma': 1.,
             'beta0': 1.,
-        })
-        fun2.run()
+        }).run()
+        self.assertTrue(10 >= t_f[1][0] - t_f[0][0])
 
     def test_algorithm_de(self):
         """Differential evolution."""
-        fun3 = Differential(self.planar_object(), {
+        _, t_f = Differential(planar_object, {
             'maxTime': 1,
             'report': 10,
             # DE
@@ -228,8 +215,8 @@ class CoreTest(TestCase):
             'NP': 400,
             'F': 0.6,
             'CR': 0.9,
-        })
-        fun3.run()
+        }).run()
+        self.assertTrue(10 >= t_f[1][0] - t_f[0][0])
 
 
 if __name__ == '__main__':
