91d02400f188c8b5139d145f25c69374a9cb8062
40323230 <daan0014119@gmail.com>
2019-02-14 17:45:54 +0800

    Update constructure of Planar class.

--- src/planar_linkage.pyx
+++ src/planar_linkage.pyx
@@ -41,7 +41,7 @@ cdef class Planar(Verification):
 
     """This class is used to verified kinematics of the linkage mechanism."""
 
-    cdef list vpoints, inputs, exprs
+    cdef list vpoints, inputs, exprs, mapping_list
     cdef dict placement, target, mapping
     cdef ndarray upper, lower
 
@@ -51,41 +51,27 @@ cdef class Planar(Verification):
             'input': [(b0, d0), ...],
             'Placement': {pt: (x, y, r)},
             'Target': {pt: [(x0, y0), (x1, y1), ...]},
+            # Bound has no position data.
             'upper': List[float],
             'lower': List[float],
         }
         """
         cdef dict placement = mech_params.get('Placement', {})
         if len(placement) == 0:
-            raise ValueError("no target")
+            raise ValueError("no grounded joint")
 
         self.target = mech_params.get('Target', {})
-        self.target_length = len(self.target)
-        if self.target_length == 0:
-            raise ValueError("no target")
+        cdef int target_length = len(self.target)
+        if target_length == 0:
+            raise ValueError("no target joint")
 
-        cdef set check_set = set(map(len, self.target.values()))
-        if len(check_set) != 1:
+        if len(set(map(len, self.target.values()))) != 1:
             raise ValueError("target paths should be in the same size")
 
         # Options
         self.vpoints = list(mech_params.get('Expression', []))
         self.inputs = list(mech_params.get('input', []))
         self.exprs = vpoints_configure(self.vpoints, self.inputs)
-
-        # Mapping
-        self.mapping = {}
-        cdef int a, b, c, d
-        cdef VLink vlink
-        for vlink in get_vlinks(self.vpoints):
-            if len(vlink.points) < 2:
-                continue
-            a = vlink.points[0]
-            b = vlink.points[1]
-            self.mapping[a, b] = 0.
-            for c in vlink.points[2:]:
-                for d in (a, b):
-                    self.mapping[c, d] = 0.
 
         # Bound
         cdef list upper = list(mech_params.get('upper', []))
@@ -96,12 +82,42 @@ cdef class Planar(Verification):
         cdef list upper_input = []
         cdef list lower_input = []
 
+        # Data mapping
         cdef int i
+        self.mapping = {i: f"P{i}" for i in range(len(self.vpoints))}
+        self.mapping_list = []
+
+        # Position
+        cdef double x, y, r
+        for i in sorted(placement):
+            x, y, r = placement[i]
+            upper_input.extend((x + r, y + r))
+            lower_input.extend((x - r, y - r))
+            self.mapping_list.append(i)
+
+        # Length of links
+        cdef int a, b, c, d
+        cdef VLink vlink
+        for vlink in get_vlinks(self.vpoints):
+            if len(vlink.points) < 2:
+                continue
+            a = vlink.points[0]
+            b = vlink.points[1]
+            self.mapping[a, b] = 0.
+            self.mapping_list.append((a, b))
+            for c in vlink.points[2:]:
+                for d in (a, b):
+                    self.mapping[c, d] = None
+                    self.mapping_list.append((c, d))
+
+        upper_input.extend(upper[:-len(self.inputs)])
+        lower_input.extend(lower[:-len(self.inputs)])
+
         for i in range(1, len(self.inputs) + 1):
-            upper_input.extend([upper[-i]] * self.target_length)
-            lower_input.extend([lower[-i]] * self.target_length)
-        self.upper = np_array(upper[:-len(self.inputs)] + upper_input, dtype=np_float32)
-        self.lower = np_array(lower[:-len(self.inputs)] + lower_input, dtype=np_float32)
+            upper_input.extend([upper[-i]] * target_length)
+            lower_input.extend([lower[-i]] * target_length)
+        self.upper = np_array(upper_input, dtype=np_float32)
+        self.lower = np_array(lower_input, dtype=np_float32)
 
         # Swap sorting.
         for i in range(len(self.upper)):
