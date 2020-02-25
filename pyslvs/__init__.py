006fd50259b472ef2d9b498e14718d12264f440b
KmolYuan <pyslvs@gmail.com>
2020-02-25 20:34:57 +0800

    Patch requirement.

--- pyslvs/__init__.py
+++ pyslvs/__init__.py
@@ -55,6 +55,7 @@ __copyright__ = "Copyright (C) 2016-2020"
 __license__ = "AGPL"
 __email__ = "pyslvs@gmail.com"
 
+import pywt as _
 from .expression import get_vlinks, VJoint, VPoint, VLink, Coordinate
 from .bfgs import SolverSystem
 from .triangulation import vpoints_configure, ExpressionStack
