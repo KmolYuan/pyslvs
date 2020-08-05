9b5c0f98875137aebcda09d3035521259b5cf23c
KmolYuan <pyslvs@gmail.com>
2020-08-05 18:42:27 +0800

    Improve the test case.

--- tests/obj_func.pyx
+++ tests/obj_func.pyx
@@ -13,18 +13,22 @@ cimport cython
 from numpy import array, float64 as np_float
 from pyslvs.metaheuristics.utility cimport ObjFunc
 
+
 @cython.final
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef class TestObj(ObjFunc):
-    """Test objective function."""
+    """Test objective function.
+
+    f(x) = x1^2 + 8x2
+    """
 
     def __cinit__(self):
         self.ub = array([100, 100], dtype=np_float)
         self.lb = array([0, 0], dtype=np_float)
 
     cdef double target(self, double[:] v):
-        cdef double x1 = v[0]
-        cdef double x2 = v[1]
-        return x1 * x1 + 8 * x2
+        return v[0] * v[0] + 8 * v[1]
 
     cdef double fitness(self, double[:] v):
         return self.target(v)
