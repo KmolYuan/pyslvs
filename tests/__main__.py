7be92e35d3d0ff5e3bbd36481cca84b29d1a02f4
KmolYuan <pyslvs@gmail.com>
2020-09-08 19:31:30 +0800

    Update submodule.

--- tests/__main__.py
+++ tests/__main__.py
@@ -256,11 +256,11 @@ class CoreTest(TestCase):
         self.algorithm_generic(AlgorithmType.TLBO)
 
     def test_obj_func(self):
-        """Test with a objective function."""
+        """Test with an objective function."""
         settings = {'min_fit': 1e-20, 'report': 10}
         obj = TestObj()
-        for t in PARAMS:
-            settings.update(PARAMS[t])
+        for t, setting in PARAMS.items():
+            settings.update(setting)
             x, fval = ALGORITHM[t](obj, settings).run()
             self.assertAlmostEqual(0., x[0], 6)
             self.assertAlmostEqual(0., x[1], 6)
