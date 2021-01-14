31a48dd6406ec6ce9b12b9c4af98bd2712334c5a
KmolYuan <pyslvs@gmail.com>
2021-01-14 11:54:46 +0800

    Apply PEP517.

--- setup.py
+++ setup.py
@@ -7,25 +7,11 @@ __copyright__ = "Copyright (C) 2016-2021"
 __license__ = "AGPL"
 __email__ = "pyslvs@gmail.com"
 
-from re import MULTILINE, search
 from os import listdir
 from os.path import sep, join as pth_join
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from platform import system
-
-
-def read(path: str):
-    with open(path, 'r', encoding='utf-8') as f:
-        return f.read()
-
-
-def find_version(path: str):
-    m = search(r"^__version__ = ['\"]([^'\"]*)['\"]", read(path), MULTILINE)
-    if m:
-        return m.group(1)
-    raise RuntimeError("Unable to find version string.")
-
 
 src_path = 'pyslvs'
 graph_path = pth_join(src_path, 'graph')
@@ -100,31 +86,4 @@ class Build(build_ext):
         super(Build, self).build_extensions()
 
 
-setup(
-    name='pyslvs',
-    version=find_version(pth_join('pyslvs', '__init__.py')),
-    author=__author__,
-    author_email=__email__,
-    license=__license__,
-    description="Pyslvs core module. "
-                "An open source planar linkage mechanism synthesis tool.",
-    long_description=read("README.md"),
-    long_description_content_type='text/markdown',
-    url="https://github.com/KmolYuan/pyslvs",
-    packages=find_packages(exclude=('test',)),
-    package_data={'': ['*.pyi', '*.pxd', '*.pyx'], 'pyslvs': ['py.typed']},
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': Build},
-    zip_safe=False,
-    python_requires=">=3.7",
-    install_requires=read('requirements.txt').splitlines(),
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Cython",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        "Operating System :: OS Independent",
-        "Typing :: Typed",
-    ]
-)
+setup(ext_modules=ext_modules, cmdclass={'build_ext': Build})
